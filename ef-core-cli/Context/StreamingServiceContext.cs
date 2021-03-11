31510ac6ceadb12b37cb519cc8a201a3e2a996c9
Maclain Wiltzer <mwiltzer@gmail.com>
2021-03-11 08:03:18 -0500

    Add context

---
+++ ef-core-cli/Context/StreamingServiceContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ef_core_cli.Context
+{
+    public class StreamingServiceContext : DbContext
+    {
+        private readonly string ConnectionString;
+        public StreamingServiceContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
