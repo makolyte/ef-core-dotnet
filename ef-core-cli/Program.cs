76477c28b03e77eb55ad3701969cc8b09d07ad36
Maclain Wiltzer <mwiltzer@gmail.com>
2021-03-12 08:26:34 -0500

    Added connection string to appsettings.json and setup user secrets

--- ef-core-cli/Program.cs
+++ ef-core-cli/Program.cs
@@ -2,11 +2,22 @@
 
 namespace ef_core_cli
 {
+    using Microsoft.Extensions.Configuration;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>()
+                .Build();
+
+
+            var connectionString = config.GetConnectionString("Default");
+
+            Console.WriteLine(connectionString);
         }
     }
 }
