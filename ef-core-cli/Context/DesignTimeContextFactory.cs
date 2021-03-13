76dea2cb0f1b6f1f862872a2f60fc89b1db20f6a
Maclain Wiltzer <mwiltzer@gmail.com>
2021-03-13 10:32:44 -0500

    Add design time context factory

---
+++ ef-core-cli/Context/DesignTimeContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ef_core_cli.Context
+{
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
+    public class DesignTimeContextFactory : IDesignTimeDbContextFactory<StreamingServiceContext>
+    {
+        public StreamingServiceContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>()
+                .Build();
+
+
+            var connectionString = config.GetConnectionString("Default");
+
+            return new StreamingServiceContext(connectionString);
+        }
+    }
+}
